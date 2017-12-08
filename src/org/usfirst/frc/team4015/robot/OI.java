c64bd743165f1a06d2faa01dd9bc7481217dfc64
Naman Gulati <gula1214@gmail.com>
2017-12-08 12:37:04 -0500

    Update OI.java

--- src/org/usfirst/frc/team4015/robot/OI.java
+++ src/org/usfirst/frc/team4015/robot/OI.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team4015.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import org.usfirst.frc.team4015.robot.RobotMap;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 /* =============================================================================
  * This class stores the objects that are used to interface with the joysticks
@@ -11,4 +12,5 @@ public class OI
 {	
 	public static Joystick leftStick = new Joystick(RobotMap.leftStickPort);
 	public static Joystick rightStick = new Joystick(RobotMap.rightStickPort);
+	public static DigitalInput limitSwitch= new DigitalInput(1);
 }
